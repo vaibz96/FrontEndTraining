3ec223075daf239dc9ffd5192d86d748686ca4b5
vaibz96 <vshanbha@stevens.edu>
2021-04-26 15:53:44 -0400

    aws-mean backend code

--- mean-stack-project/backend/app.js
+++ mean-stack-project/backend/app.js
@@ -8,6 +8,15 @@ let cors = require("cors");
 //Database URL Details 
 let url = "mongodb://localhost:27017/meanstack";
 
+//unable to load the static file
+app.use(express.static(process.cwd()));
+
+
+// load the frontend file in angular program
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
 //middleware enable data from post method.
 app.use(bodyParser.urlencoded({extended:true}));    // enable body part data  
 app.use(bodyParser.json());                         // json data. 
--- mean-stack-project/frontend/product-crud-operation/src/app/product.service.ts
+++ mean-stack-project/frontend/product-crud-operation/src/app/product.service.ts
@@ -7,29 +7,30 @@ import { Product } from './model.product';
   providedIn: 'root'
 })
 export class ProductService {
+  ipAddress:string="http://34.239.141.87:9090";
 
   constructor(public http:HttpClient) { }
   //post methoid 1st parameter and 2nd parameter json data
 
   storeProductDetailsInfo(productRef:any){
-    this.http.post("http://localhost:9090/product/storeProductDetails", productRef).
+    this.http.post(this.ipAddress+"/product/storeProductDetails", productRef).
     subscribe(result=>console.log(result), error=>console.log(error));
   }
 
   retrieveAllProductsDetails():Observable<Product[]>{
-    return this.http.get<Product[]>("http://localhost:9090/product/allProductDetails")
+    return this.http.get<Product[]>(this.ipAddress+"/product/allProductDetails")
   }
 
   retrieveProductById(id:any):Observable<Product[]>{
-    return this.http.get<Product[]>("http://localhost:9090/product/retrieveProductById"+id)
+    return this.http.get<Product[]>(this.ipAddress+"/product/retrieveProductById"+id)
   }
 
   deleteProductById(id:any):any{
-    return this.http.delete("http://localhost:9090/product/deleteProductById/"+id, {responseType:'text'});
+    return this.http.delete(this.ipAddress+"/product/deleteProductById/"+id, {responseType:'text'});
   }
 
   updateProductPrice(productRef:any){
-    return this.http.put("http://localhost:9090/product/updateProductPrice", productRef, {responseType:"text"});
+    return this.http.put(this.ipAddress+"/product/updateProductPrice", productRef, {responseType:"text"});
   }
 
 }
